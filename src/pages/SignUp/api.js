716ca6a671f84e5226f226c3368d89b160f0dfbe
hllbr <halibrahim.kocak@gmail.com>
2024-01-19 00:38:14 +0300

    i18n library integrated in project and refactor files

--- src/pages/SignUp/api.js
+++ src/pages/SignUp/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
+import { i18nInstance } from "../../local";
 
 const BASE_URL = "/api/v1/users";
 
 export const signUp = (body) => {
-    return axios.post(BASE_URL,body);
+  return axios.post(BASE_URL, body, {
+    headers: {
+      "Accept-Language": i18nInstance.language
+    },
+  });
 };
