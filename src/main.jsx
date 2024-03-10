32ef7cec08418ec3405a5ea980a0fc76b1ee5c8a
hllbr <halibrahim.kocak@gmail.com>
2024-03-10 03:56:31 +0300

    axios intersepter used and scroll modified,file editing

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SingUp } from "./pages/SignUp/index.jsx";
 import "./styles.scss";
 import "./local/index.js";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home } from "./pages/Home/index.jsx";
-import { Unexpected } from "./pages/unexpected/unexpceted.jsx";
-import App from "./App.jsx";
-import { Activation } from "./pages/activation/index.jsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/index.js";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: App,
-    children: [
-      {
-        path: "*",
-        Component: Home,
-        errorElement: <Unexpected />,
-      },
-      {
-        path: "/signup",
-        Component: SingUp,
-        errorElement: <Unexpected />,
-      },
-      {
-        path: "/activation/:token",
-        Component: Activation,
-      },
-    ],
-  },
-]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   // strictMode is a structure that allows to render useEffect twice and works in development mode.Producta usage is not very preferred
   //  <React.StrictMode>
