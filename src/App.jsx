5583346dc0efc3198d4559f8a30ff2ba593872d5
hllbr <halibrahim.kocak@gmail.com>
2024-03-17 01:47:35 +0300

    userPage created and file editing,common components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { LanguageSelector } from "./shared/components/LanguageSelector";
 import { Navbar } from "./shared/components/NavBar";
 const App = () => {
