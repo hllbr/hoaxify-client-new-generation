86ef6f908b333879932ced450bbee1dba0c2be2e
hllbr <halibrahim.kocak@gmail.com>
2024-04-17 18:19:54 +0300

    global state

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,93 +1,94 @@
-import { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { Input } from "@/shared/components/Input";
-import { Alert } from "@/shared/components/Alert";
-import Spinner from "@/shared/components/Spinner";
-import { Button } from "@/shared/components/Button";
-import { login } from "./api";
+import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Input } from '@/shared/components/Input'
+import { Alert } from '@/shared/components/Alert'
+import Spinner from '@/shared/components/Spinner'
+import { Button } from '@/shared/components/Button'
+import { login } from './api'
 
-export const Login = () => {
+export const Login = ({ onLoginSuccess }) => {
   //#region States
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [apiProgress, setApiProgress] = useState(false);
-  const [errors, setErrors] = useState({});
-  const [generalError, setGeneralError] = useState("");
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const [apiProgress, setApiProgress] = useState(false)
+  const [errors, setErrors] = useState({})
+  const [generalError, setGeneralError] = useState('')
   //#endregion
 
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   useEffect(() => {
-    setErrors((lastErrors) => {
-      return { ...lastErrors, email: undefined };
-    });
-  }, [email]);
+    setErrors(lastErrors => {
+      return { ...lastErrors, email: undefined }
+    })
+  }, [email])
 
   useEffect(() => {
-    setErrors((lastErrors) => {
-      return { ...lastErrors, password: undefined };
-    });
-  }, [password]);
+    setErrors(lastErrors => {
+      return { ...lastErrors, password: undefined }
+    })
+  }, [password])
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    setGeneralError();
-    setApiProgress(true);
+  const onSubmit = async event => {
+    event.preventDefault()
+    setGeneralError()
+    setApiProgress(true)
     try {
-      await login({ email, password })
+      const response = await login({ email, password })
+      onLoginSuccess(response.data.user)
     } catch (axiosError) {
-        if (axiosError?.response?.data) {
-          if (axiosError?.response?.data?.status === 400) {
-            setErrors(axiosError?.response?.data?.validationErrors);
-          } else {
-            setGeneralError(axiosError?.response?.data?.message);
-          }
+      if (axiosError?.response?.data) {
+        if (axiosError?.response?.data?.status === 400) {
+          setErrors(axiosError?.response?.data?.validationErrors)
         } else {
-          setGeneralError(t("Unexpected error occured. Please try again"));
+          setGeneralError(axiosError?.response?.data?.message)
         }
+      } else {
+        setGeneralError(t('Unexpected error occured. Please try again'))
+      }
     } finally {
-        setApiProgress(false);
+      setApiProgress(false)
     }
-  };
+  }
 
   return (
-    <div className="container">
-      <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
-        <form className="card" onSubmit={onSubmit}>
-          <div className="text-center card-header">
-            <h1>{t("Login.title")}</h1>
+    <div className='container'>
+      <div className='col-lg-6 offset-lg-3 col-sm-8 offset-sm-2'>
+        <form className='card' onSubmit={onSubmit}>
+          <div className='text-center card-header'>
+            <h1>{t('Login.title')}</h1>
           </div>
-          <div className="card-body">
+          <div className='card-body'>
             <Input
-              id="email"
-              label={t("signupPage.email")}
+              id='email'
+              label={t('signupPage.email')}
               error={errors.email}
-              onChange={(event) => {
-                setEmail(event.target.value);
+              onChange={event => {
+                setEmail(event.target.value)
               }}
             />
             <Input
-              id="password"
-              label={t("signupPage.password")}
+              id='password'
+              label={t('signupPage.password')}
               error={errors.password}
-              type={"password"}
-              onChange={(event) => {
-                setPassword(event.target.value);
+              type={'password'}
+              onChange={event => {
+                setPassword(event.target.value)
               }}
             />
             {generalError && (
-              <Alert message={generalError} styleType="danger" />
+              <Alert message={generalError} styleType='danger' />
             )}
-            <div className="text-center">
+            <div className='text-center'>
               <Button
                 disabled={!email || !password}
                 apiProgress={apiProgress}
-                text={t("Login.title")}
+                text={t('Login.title')}
               />
             </div>
           </div>
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
