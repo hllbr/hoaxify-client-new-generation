bc8e37ec3b91bf5def18f17dcda031c22d7e59eb
hllbr <halibrahim.kocak@gmail.com>
2024-04-18 23:59:53 +0300

    username edit bugfix operation

--- src/pages/Home/Components/UserListItem.jsx
+++ src/pages/Home/Components/UserListItem.jsx
@@ -1,20 +1,16 @@
-import { useState } from "react";
-import defaultProfileImage from "@/assets/profile.png";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import { ProfileImage } from '@/shared/components/ProfileImage'
+
 export const UserListItem = ({ user }) => {
   //   const [selectedUser, setSelectedUser] = useState(false);
   return (
     <Link
       to={`/user/${user.id}`}
-      className="list-group-item list-group-item-action"
-      style={{ textDecoration: "none" }}
+      className='list-group-item list-group-item-action'
+      style={{ textDecoration: 'none' }}
     >
-      <img
-        src={defaultProfileImage}
-        width={30}
-        className="img-fluid rounded-circle shadow-sm me-2"
-      />
-      <span className="ms-2">{user.username}</span>
+      <ProfileImage width={30} />
+      <span className='ms-2'>{user.username}</span>
     </Link>
-  );
-};
+  )
+}
--- src/pages/User/components/ProfileCard/index.jsx
+++ src/pages/User/components/ProfileCard/index.jsx
@@ -1,69 +1,33 @@
-
-import defaultProfileImage from '@/assets/profile.png'
 import { Button } from '@/shared/components/Button'
-import { Input } from '@/shared/components/Input'
 import { useAuthState } from '@/shared/state/context'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { updateUser } from './api'
+import { ProfileImage } from '@/shared/components/ProfileImage'
+import { UserEditForm } from './UserEditForm'
 
 export const ProfileCard = ({ user }) => {
   const authState = useAuthState()
   const { t } = useTranslation()
   const [editMode, setEditMode] = useState(false)
   const isEditButtonVisible = !editMode && authState.id === user?.id
-  const [newUsername, setNewUsername] = useState()
-  const [apiProgress, setApiProgress] = useState(false)
 
-  const onChangeUsername = event => {
-    setNewUsername(event.target.value)
-  }
-
-  const onClickSave = async () => {
-    setApiProgress(true)
-    try {
-      await updateUser(user.id, { username: newUsername })
-    } catch {
-      
-    } finally {
-      setApiProgress(false)
-    }
-  }
+  const visibileUsername =
+    authState.id === user.id ? authState.username : user.username
 
   return (
     <div className='card'>
       <div className='card-header text-center'>
-        <img
-          src={defaultProfileImage}
-          width={200}
-          className='img-fluid rounded-circle shadow-sm me-2'
-        />
+        <ProfileImage width={200} />
       </div>
       <div className='card-body text-center'>
-        {!editMode && <span className='fs-3 d-block'>{user.username}</span>}
+        {!editMode && <span className='fs-3 d-block'>{visibileUsername}</span>}
         {isEditButtonVisible && (
           <Button
             onClick={() => setEditMode(true)}
             text={t('Profile.editProfile')}
           />
         )}
-        {editMode && (
-          <>
-            <Input
-              onChange={onChangeUsername}
-              defaultValue={user.username}
-              label={t('Profile.username')}
-            />
-            <Button apiProgress={apiProgress} disabled={apiProgress} onClick={onClickSave} text={t('Profile.save')} />
-            <div className='d-inline m-2'></div>
-            <Button
-              disabled={apiProgress}
-              onClick={() => setEditMode(false)}
-              className={'btn btn-outline-secondary'}
-              text={t('Profile.cancel')}
-            />
-          </>
-        )}
+        {editMode && <UserEditForm setEditMode={setEditMode} />}
       </div>
     </div>
   )
